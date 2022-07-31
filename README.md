ab4e400ecd3473e68973b29fe0e23ef9c430cf9e
Inder Panesar <Inder_Panesar@hotmail.com>
2022-07-31 01:26:38 +0100

    Basic Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# basketball_sim_app
+
+Creating a Basketball Simulator Game for becoming the GM of a team, simulating the day to day process.
---
+++ android/app/src/main/kotlin/com/fantasiar/basketball_sim_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fantasiar.basketball_sim_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
